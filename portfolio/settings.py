6dd25b8f04c623715dab063d001746a855602989
rohithselva <rohithselva1@gmail.com>
2026-06-21 21:07:19 +0530

    Update DEBUG setting to True in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-in-production')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
